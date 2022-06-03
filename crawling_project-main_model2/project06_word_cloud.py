863628db8481de6377466cde3180aff035a240ee
WhiteParasols <whiteparasol108@gmail.com>
2022-06-03 09:25:27 +0900

    project06으로 word cloud

--- crawling_project-main_model2/project06_word_cloud.py
+++ crawling_project-main_model2/project06_word_cloud.py
@@ -1,13 +1,8 @@
 import pandas as pd  # 결과값을 데이터프레임 객체로 저장하기 위해 이용
-
 from wordcloud import WordCloud
-
 import matplotlib.pyplot as plt  # 그래프 그리기
-
 from collections import Counter  # 데이터의 개수를 정리할 수 있는 모듈
-
 from konlpy.tag import Kkma  # 형태소 분석기 호출
-
 from konlpy.utils import pprint  # 유니코드 문자 출력
 
 from selenium import webdriver  # 브라우저 제어
@@ -16,7 +11,6 @@ from selenium import webdriver  # 브라우저 제어
 new = pd.read_csv('./crawling_data(2)/wadiz_data(2)_20220530.csv')  # 여기 주소값만 바꿔주세요!
 
 # csv 파일 불러오기 (위치 임의 지정)
-
 
 kkma = Kkma()
 nouns_list = []  # nouns_list생성
